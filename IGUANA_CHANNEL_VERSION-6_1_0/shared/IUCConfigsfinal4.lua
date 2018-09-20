935aa6dfa36273b5e018f34010107aba8e2c3171
iamfinal4 <iamfinal4>
2018-09-20 03:44:46 +0000

    Export: dev changes

--- IGUANA_CHANNEL_VERSION-6_1_0/shared/IUCConfigsfinal4.lua
+++ IGUANA_CHANNEL_VERSION-6_1_0/shared/IUCConfigsfinal4.lua
@@ -1,4 +1,4 @@
-local groupName = 'GROUPNAME'
+local groupName = 'final4'
 local envVariable = 'serverType'..groupName
 local serverType = os.getenv(envVariable)
 
@@ -6,22 +6,22 @@ local iucConf = {}
 
 iucConf.dev = {
    -- Configurations for patient csv File
-   ['patientCsvPath'] = '/home/centos/iuc/GROUPNAME/',
+   ['patientCsvPath'] = '/home/centos/iuc/final4/',
    ['patientCsvFile'] = 'PatientData.txt',
 	
    -- Configurations for patient DB file
-   ['patientDbPath'] = '/home/centos/iuc/GROUPNAME/',
+   ['patientDbPath'] = '/home/centos/iuc/final4/',
    ['patientDbFile'] = 'PatientData.sqlite'
 }
 
 
 iucConf.prod = {
    -- Configurations for patient csv File
-   ['patientCsvPath'] = '/home/centos/iuc/prod/GROUPNAME/',
+   ['patientCsvPath'] = '/home/centos/iuc/prod/final4/',
    ['patientCsvFile'] = 'PatientData.txt',
 	
    -- Configurations for patient DB file
-   ['patientDbPath'] = '/home/centos/iuc/prod/GROUPNAME/',
+   ['patientDbPath'] = '/home/centos/iuc/prod/final4/',
    ['patientDbFile'] = 'PatientData.sqlite'
 }
 
